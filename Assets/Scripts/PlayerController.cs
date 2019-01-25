4b69d30b59f6a959d4b89241d0725b5f71830ae2
Joel Lopez <jowie.jowie@gmail.com>
2019-01-26 00:06:19 +0100

    Implement cold mechanic with speed reduction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerLifeController))]
 public class PlayerController : MonoBehaviour
 {
     public float mSpeed = 1.0f;
+    public float MinSpeedFactor = 0.4f;
+    public float MaxSpeedFactor = 1.0f;
 
     private Rigidbody mRigidbody;
+    private PlayerLifeController playerLife;
 
     // Start is called before the first frame update
     void Start()
     {
         mRigidbody = GetComponent<Rigidbody>();
         Assert.IsNotNull(mRigidbody);
+
+        playerLife = GetComponent<PlayerLifeController>();
+        Assert.IsNotNull(playerLife);
     }
 
     // Update is called once per frame
@@ -24,11 +31,18 @@ public class PlayerController : MonoBehaviour
 
         if ( (lMoveHorizontal) != 0.0f || (lMoveVertical != 0.0f) )
         {
-            mRigidbody.velocity = Vector3.Normalize( new Vector3(lMoveHorizontal, 0.0f, lMoveVertical) ) * mSpeed * Time.deltaTime;
+            mRigidbody.velocity = Vector3.Normalize( new Vector3(lMoveHorizontal, 0.0f, lMoveVertical) ) * GetSpeed() * Time.deltaTime;
         }
         else
         {
             mRigidbody.velocity = Vector3.zero;
         }
+    }
+
+    private float GetSpeed()
+    {
+        // TODO: Maybe use a Bezier curve
+        float speedFactor = Mathf.Lerp(MinSpeedFactor, MaxSpeedFactor, playerLife.Life * 0.01f);
+        return speedFactor * mSpeed;
     }
 }
