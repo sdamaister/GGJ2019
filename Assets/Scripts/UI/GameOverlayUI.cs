cf433212a0859e2c2b0b613a7734b1696cf20832
Joel Lopez <jowie.jowie@gmail.com>
2019-01-27 10:26:54 +0100

    Add Game UI screen overlay

---
+++ Assets/Scripts/UI/GameOverlayUI.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using UnityEngine;
+
+public class GameOverlayUI : MonoBehaviour
+{
+    enum OverlayStates
+    {
+        Hidden,
+        Showing,
+        Shown,
+        Disappearing
+    }
+
+    public float FadeDuration = 1.5f;
+    public float ShowTime = 1.5f;
+
+    public event Action OnAnimationFinished;
+
+    [SerializeField] private RoundIndicatorUI roundIndicator;
+    private CanvasGroup canvasGroup;
+
+    private OverlayStates state = OverlayStates.Hidden;
+    private float remainingFade = 0.0f;
+
+    public void ShowRound(int round, int player)
+    {
+        BeginFade(roundIndicator.gameObject);
+        roundIndicator.ShowRound(round, player);
+    }
+
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        Canvas canvas = GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = Camera.main;
+    }
+
+    void BeginFade(GameObject shownObject)
+    {
+        foreach (var child in transform)
+        {
+            ((Transform)child).gameObject.SetActive(false);
+        }
+
+        shownObject.SetActive(true);
+        canvasGroup.alpha = 0.0f;
+
+        remainingFade = FadeDuration;
+        state = OverlayStates.Showing;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case OverlayStates.Showing:
+            {
+                remainingFade -= Time.deltaTime;
+
+                canvasGroup.alpha = Mathf.Lerp(1.0f, 0.0f, remainingFade / FadeDuration);
+
+                if (remainingFade <= 0.0f)
+                {
+                    remainingFade = ShowTime;
+                    state = OverlayStates.Shown;
+                }
+
+                break;
+            }
+            case OverlayStates.Shown:
+            {
+                remainingFade -= Time.deltaTime;
+                if (remainingFade <= 0.0f)
+                {
+                    remainingFade = FadeDuration;
+                    state = OverlayStates.Disappearing;
+                }
+
+                break;
+            }
+            case OverlayStates.Disappearing:
+            {
+                remainingFade -= Time.deltaTime;
+
+                canvasGroup.alpha = Mathf.Lerp(0.0f, 1.0f, remainingFade / FadeDuration);
+
+                if (remainingFade <= 0.0f)
+                {
+                    state = OverlayStates.Hidden;
+
+                    if (OnAnimationFinished != null)
+                    {
+                        OnAnimationFinished();
+                    }
+                }
+
+                break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/RoundIndicatorUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundIndicatorUI : MonoBehaviour
+{
+    public Image RoundImage;
+
+    public void ShowRound(int round, int winner)
+    {
+        string spriteName = "Rounds/round" + round;
+        Texture2D texture = (Texture2D)Resources.Load(spriteName);
+        Rect rect = new Rect(0, 0, texture.width, texture.height);
+
+        RoundImage.sprite = Sprite.Create(texture, rect, Vector2.zero);
+        RoundImage.preserveAspect = true;
+    }
+}
