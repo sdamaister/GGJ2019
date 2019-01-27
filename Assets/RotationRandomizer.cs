305b84041f1ce79f601defd732f934a983e5acf4
David da Silva <dasilvacontin@gmail.com>
2019-01-27 07:18:37 +0100

    add clara's tree and more trees to the scene

---
+++ Assets/RotationRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationRandomizer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+    }
+}
