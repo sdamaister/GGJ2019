8e68ea50b0d31bc32de3aa470b83dd7dbde5441c
Joel Lopez <jowie.jowie@gmail.com>
2019-01-27 14:13:59 +0100

    Create Sound Controller

---
+++ Assets/Scripts/SoundController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SoundController : MonoBehaviour
+{
+    public AudioClip[] Mine;
+    public AudioClip[] Throw;
+    public AudioClip[] GrabStick;
+    public AudioClip[] GrabTorch;
+    public AudioClip[] Stun;
+    public AudioClip[] DieOfHeat;
+
+    private AudioSource emitter;
+
+    public float PlayMine()
+    {
+        return PlayAudio(SelectRandomClip(Mine));
+    }
+
+    public float PlayThrow()
+    {
+        return PlayAudio(SelectRandomClip(Throw));
+    }
+
+    public float PlayGrabStick()
+    {
+        return PlayAudio(SelectRandomClip(GrabStick));
+    }
+
+    public float PlayGrabTorch()
+    {
+        return PlayAudio(SelectRandomClip(GrabTorch));
+    }
+
+    public float PlayStun()
+    {
+        return PlayAudio(SelectRandomClip(Stun));
+    }
+
+    public float PlayDieOfHeat()
+    {
+        return PlayAudio(SelectRandomClip(DieOfHeat));
+    }
+
+    private AudioClip SelectRandomClip(AudioClip[] list)
+    {
+        if (list.Length == 0)
+        {
+            return null;
+        }
+
+        int pos = Random.Range(0, list.Length);
+
+        return list[pos];
+    }
+
+    private float PlayAudio(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            emitter.PlayOneShot(clip);
+            return clip.length;
+        }
+
+        return 0;
+    }
+}
