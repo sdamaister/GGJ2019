c3933f6d0b13bba1f93696be3a9870e9de2d5984
Cristian Rodríguez Vázquez <sda.maister@gmail.com>
2019-01-27 15:13:16 +0100

    Tutorial image

---
+++ Assets/Scripts/UI/Tutorial.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour
+{
+    Texture mTexture;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Create Canvas GameObject.
+        GameObject lCanvasGO = new GameObject();
+        lCanvasGO.name = "Canvas";
+        lCanvasGO.AddComponent<Canvas>();
+        lCanvasGO.AddComponent<CanvasScaler>();
+        lCanvasGO.AddComponent<GraphicRaycaster>();
+
+        // Get canvas from the GameObject.
+        Canvas lCanvas;
+        lCanvas = lCanvasGO.GetComponent<Canvas>();
+        lCanvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
